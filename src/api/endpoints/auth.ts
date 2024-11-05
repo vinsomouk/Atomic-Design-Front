d83f07f67ce52bdc477b1907f922be9c39381074
vinsomouk <diakitekhalidou1@gmail.com>
2024-11-05 14:45:22 +0100

    Mise à jour du store

--- src/api/endpoints/auth.ts
+++ src/api/endpoints/auth.ts
@@ -1,11 +1,12 @@
+// src/services/api.ts
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const api = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000/api/login_check' }), 
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000/api' }), // URL de base sans le chemin de l'endpoint
     endpoints: (builder) => ({
         checkAuth: builder.query<any, void>({
-            query: () => '/me', // Endpoint pour vérifier l'utilisateur
+            query: () => '/login_check/me', // Endpoint pour vérifier l'utilisateur
         }),
     }),
 });
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,16 @@
 // src/store.ts
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
+import api from '../api/endpoints/auth';
 
 // Configuration du store Redux
 const store = configureStore({
     reducer: {
-        auth: authReducer, // Ajoutez le reducer d'authentification ici
-        // Vous pouvez ajouter d'autres reducers ici si nécessaire
+        auth: authReducer, // Reducer d'authentification
+        [api.reducerPath]: api.reducer, // Ajoutez le reducer de l'API ici
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(api.middleware), // Ajoutez le middleware de l'API
 });
 
 // Export du store
